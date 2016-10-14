09db59d13e4580c8c99a298960c61ef14c68f111
tonnth <tonnth@hotmail.com>
2016-10-14 22:11:53 +0700

    First

---
+++ SceneMain.cpp
@@ -0,0 +1,27 @@
+
+#include "SceneMain.h"
+
+SceneMain::SceneMain(int _nCmdShow): CGame(_nCmdShow)
+{
+}
+
+void SceneMain::RenderFrame(LPDIRECT3DDEVICE9 d3ddv, int t)
+{
+}
+
+void SceneMain::ProcessInput(LPDIRECT3DDEVICE9 d3ddv, int Delta)
+{
+}
+
+void SceneMain::LoadResources(LPDIRECT3DDEVICE9 d3ddv)
+{
+}
+
+void SceneMain::OnKeyDown(int KeyCode)
+{
+
+}
+
+SceneMain::~SceneMain(void)
+{
+}
